1c0170daee4ef37eeb7bfe2f7c922bd306da79d4
CaptainSara <sara.iqbal@hotmail.se>
2022-12-06 11:22:23 +0100

    StartPage, Header, Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
-import {BrowserRoutes, Routes, Route} from 'react-dom'
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import StartPage from './pages/StartPage'
+import Header from './components/Header'
 
 
-
-function App() {
-  
-
-  return 
-  <div>Sara</div>
+export default function App() {
+  return (
+    <main>
+      <h1>Sara</h1>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+          <Route path="/" element={ <StartPage /> } />
+          <Route path="/H" element={<Header/>}/>
+      </Routes>
+      </BrowserRouter>
+      </main>
+    )
 }
 
-export default App
---
+++ src/pages/StartPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function StartPage() {
+  
+  return (
+    <section class="bgimage" id="home">
+        <div class="container-fluid">
+            <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 hero-text">
+                <h2 class="hero_title">Hi, it's me Sara</h2>
+                <p class="hero_desc">I am a professional freelancer in New York City</p>
+            </div>
+            </div>
+        </div>
+    </section>
+  )
+}
